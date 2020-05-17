973e8b9ca33ad2768858e7b1236b08ef81ce82e8
Mirkoyk <54212139+Mirkoyk@users.noreply.github.com>
2020-05-17 14:14:16 +0200

    first commit

---
+++ precache-manifest.3f2398a11309eba4926cc6af385b0b95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87750fb40d6d02b72b1e8aca1667bff6",
+    "url": "/daily-covid-tracker/index.html"
+  },
+  {
+    "revision": "7acb879340fa367161b8",
+    "url": "/daily-covid-tracker/static/css/2.cc73468a.chunk.css"
+  },
+  {
+    "revision": "58fb6266d94049d148b4",
+    "url": "/daily-covid-tracker/static/css/main.bb221544.chunk.css"
+  },
+  {
+    "revision": "7acb879340fa367161b8",
+    "url": "/daily-covid-tracker/static/js/2.2c441a92.chunk.js"
+  },
+  {
+    "revision": "dea80da3cf5f4dacd14f2aa5c1d608ac",
+    "url": "/daily-covid-tracker/static/js/2.2c441a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58fb6266d94049d148b4",
+    "url": "/daily-covid-tracker/static/js/main.73b9d50b.chunk.js"
+  },
+  {
+    "revision": "01ee2915fe0098b114f3",
+    "url": "/daily-covid-tracker/static/js/runtime-main.9ac2f0ab.js"
+  }
+]);
